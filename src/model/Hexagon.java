bec7daf636417d7d4cd3d25b526f8fa27e2089b7
Jordan Frazier <jrf30@duke.edu>
2016-09-23 15:50:27 -0400

    Rendering changes to hexagon and fish

--- src/model/Hexagon.java
+++ src/model/Hexagon.java
@@ -1,8 +1,20 @@
 package model;
+
+import javafx.scene.Node;
+import javafx.scene.shape.Polygon;
+
 public class Hexagon extends Cell {
 
 	private int[] rowDeltas = {-1, 0, 1, 0, 1, 1, -1, -1};
 	private int[] colDeltas = {0, -1, 0, 1, 1, -1, 1, -1};
+	
+	private Double[] hexagonPoints = {10.0, 10.0,
+			10.0, 10.0,
+			20.0, 20.0,
+			30.0, 30.0, 
+			10.0 , 10.0, 
+			20.0, 12.0
+	};
 	
 	public Hexagon(int row, int col, boolean isEven) {
 		super(row, col);
@@ -10,7 +22,10 @@ public class Hexagon extends Cell {
 
 	@Override
 	public Node render() {
-		// yeah
+		Polygon hexagon = new Polygon();
+		hexagon.getPoints().addAll(hexagonPoints);
+		
+		return hexagon;
 	}
 
 	@Override
