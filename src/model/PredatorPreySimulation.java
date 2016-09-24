0504614dfebe8a206b24518f330868a46ce2ccf7
Austin <aeg36@duke.edu>
2016-09-23 23:32:47 -0400

    Working PredatorPrey Sim

--- src/model/PredatorPreySimulation.java
+++ src/model/PredatorPreySimulation.java
@@ -9,37 +9,15 @@ public class PredatorPreySimulation extends CellGrid {
 	public static final String EMPTY = "EMPTY";
 	public static final String FISH = "FISH";
 	public static final String SHARK = "SHARK";
-	private int energyIncrease = 10;
+	//private static final int energyIncrease = 10;
 	private Random generator;
-
-	// RectangleNoDiagonals[][] myGrid;
-
-	// instead of changing the state, do I want to physically make a copy of my
-	// cell?
-	// because needs to retain reproduction time and time to birth etc.
-
-	@Override
-	public void updateGrid() {
-		// TODO Auto-generated method stub
-
-	}
 
 	public PredatorPreySimulation(int rows, int cols) {
 		super(rows, cols);
-		// super();
-		// myGrid = new RectangleNoDiagonals[rows][cols];
 	}
 
-	// public int getNumRows() {
-	// return myGrid.length;
-	// }
-	//
-	// public int getNumCols() {
-	// return myGrid[0].length;
-	// }
-
 	public void createGrid() {
-		Random generator = new Random();
+		generator = new Random();
 		for (int i = 0; i < getNumRows(); i++) {
 			for (int j = 0; j < getNumCols(); j++) {
 				int deadChance = generator.nextInt(3);
@@ -56,33 +34,104 @@ public class PredatorPreySimulation extends CellGrid {
 
 	public void intializeState(int row, int col, String state) {
 		Cell[][] myGrid = getGrid();
-		myGrid[row][col] = new RectangleNoDiagonals(row, col);
+		myGrid[row][col] = new Fish(row, col, 2, 10);
 		myGrid[row][col].setCurrentstate(state);
 		myGrid[row][col].setFuturestate("");
 	}
+	@Override
+	public void updateGrid(){
+		Cell[][] myGrid = getGrid();
+		updateFutureStates();
+		for (int i = 0; i < getNumRows(); i++) {
+			for (int j = 0; j < getNumCols(); j++) {
+				Cell currentCell = myGrid[i][j];
+				if(currentCell.getFuturestate().equals("")){
+					currentCell.setFuturestate(EMPTY);
+				}
+				currentCell.setCurrentstate(currentCell.getFuturestate());
+				currentCell.setFuturestate("");
+			}
+		}
+	}
+	
+	public void updateFutureStates() {
+		Cell[][] myGrid = getGrid();
+		for (int i = 0; i < getNumRows(); i++) {
+			for (int j = 0; j < getNumCols(); j++) {
+				Cell currentCell = myGrid[i][j];
+				if(currentCell.getCurrentstate().equals(SHARK)){
+					updateCell(currentCell);
+				}
+			}
+		}	
+		for (int i = 0; i < getNumRows(); i++) {
+			for (int j = 0; j < getNumCols(); j++) {
+				Cell currentCell = myGrid[i][j];
+				if(!currentCell.getCurrentstate().equals(SHARK)){
+					updateCell(currentCell);
+				}
+			}
+		}
+	}
+	
 
 	@Override
 	public void updateCell(Cell myCell) {
-		if (myCell.getCurrentstate().equals(SHARK)) {
-
+		if(myCell.getFuturestate().equals("") && !myCell.getCurrentstate().equals(EMPTY)){
+			moveCell(myCell);
 		}
 	}
 
-	private void moveShark(Cell myShark) {
-		ArrayList<Cell> myFishFriends = getFishNeighbors(myShark);
-		if (myFishFriends.size() > 0) {
-			Cell newSharkCell = getNewCell(myFishFriends);
-			;
-			transferInformation((Fish) myShark, (Fish) newSharkCell);
-		} else {
-			ArrayList<Cell> availableCells = getAvailableCells(myShark);
-			if (availableCells.size() == 0) {
-				noMove((Fish) myShark);
+	private void moveCell(Cell myCreature) {
+		String state = myCreature.getCurrentstate();
+		ArrayList<Cell> myFishFriends = getFishNeighbors(myCreature);
+		if(state.equals(SHARK)){
+			if (myFishFriends.size() > 0) {
+				Cell newSharkCell = getNewCell(myFishFriends);
+				//boolean to decide whether or not the shark ate
+				transferInformation((Fish) myCreature, (Fish) newSharkCell, true);
+				if(((Fish) myCreature).getReproductionTime()<=0){
+					giveBirth((Fish) myCreature, SHARK);
+				}
 				return;
 			}
-			Cell newSharkCell = getNewCell(availableCells);
-			transferInformation((Fish) myShark, (Fish) newSharkCell);
+			//kill shark if it runs out of life
+			if(((Fish) myCreature).getTimeToDeath() == 1){
+				myCreature.setFuturestate(EMPTY);
+				return;
+			}
 		}
+		ArrayList<Cell> availableCells = getAvailableCells(myCreature);
+		if (availableCells.size() == 0) {
+			noMove((Fish) myCreature);
+			return;
+		}
+		Cell newCreatureCell = getNewCell(availableCells);
+		//fish moving to same cell as a shark and getting eaten
+		if(newCreatureCell.getFuturestate().equals(SHARK)){
+			((Fish) newCreatureCell).increaseTimeToDeath();
+			if(((Fish) myCreature).getReproductionTime()<=0){
+				giveBirth((Fish) myCreature, FISH);
+			}
+			return;
+		}
+		transferInformation((Fish) myCreature, (Fish) newCreatureCell, false);
+		if(((Fish) myCreature).getReproductionTime()<=0){
+			if(state.equals(SHARK)){
+				giveBirth((Fish) myCreature, SHARK);
+				((Fish)newCreatureCell).resetReproductionTime();
+			}
+			else{
+				giveBirth((Fish) myCreature, FISH);
+				((Fish)newCreatureCell).resetReproductionTime();
+			}		
+		}
+	}
+	
+	public void giveBirth(Fish myCell, String state){
+		 myCell.setFuturestate(state); 
+		 myCell.resetReproductionTime();
+		 myCell.resetTimeToDeath(); 
 	}
 
 	private Cell getNewCell(ArrayList<Cell> availableCells) {
@@ -103,18 +152,29 @@ public class PredatorPreySimulation extends CellGrid {
 		ArrayList<Cell> neighbors = getNeighbors(myCreature);
 		ArrayList<Cell> availableCells = new ArrayList<Cell>();
 		for (Cell neighbor : neighbors) {
-			if (neighbor.getCurrentstate().equals("")
-					&& !neighbor.getFuturestate().equals(myCreature.getCurrentstate())) {
+			if (neighbor.getCurrentstate().equals(EMPTY) &&
+					!neighbor.getFuturestate().equals(myCreature.getCurrentstate())){
 				availableCells.add(neighbor);
 			}
 		}
 		return availableCells;
 	}
 
-	public void transferInformation(Fish prevCell, Fish newCell) {
-		newCell.setReproductionTime(prevCell.getReproductionTime());
+	public void transferInformation(Fish prevCell, Fish newCell, boolean didEat) {
+		newCell.setReproductionTime(prevCell.getReproductionTime()-1);
 		newCell.setMaxReproductionTime(prevCell.getMaxReproductionTime());
-		newCell.setTimeToDeath(prevCell.getTimeToDeath());
+		if(prevCell.getCurrentstate().equals(SHARK)){
+			if(didEat){
+				prevCell.increaseTimeToDeath();
+				newCell.setTimeToDeath(prevCell.getTimeToDeath());
+			}
+			else{
+				newCell.setTimeToDeath(prevCell.getTimeToDeath()-1);
+			}
+		}
+		else{
+			newCell.setTimeToDeath(prevCell.getTimeToDeath());
+		}
 		newCell.setFuturestate(prevCell.getCurrentstate());
 	}
 
@@ -129,70 +189,34 @@ public class PredatorPreySimulation extends CellGrid {
 		return myFishFriends;
 	}
 
-	/*
-	 * public void updateCell(Fish myCell){ generator = new Random(); String
-	 * state = myCell.getCurrentstate(); ArrayList<RectangleNoDiagonals>
-	 * neighbors = myCell.getNeighbors(myCell, myGrid); if(state == SHARK){
-	 * sharkMove(neighbors, myCell); } }
-	 * 
-	 * public void fishMove(ArrayList<RectangleNoDiagonals> myNeighbors, Fish
-	 * myCell){ int numEmptyNeighbors = countEmpty(myNeighbors); int direction;
-	 * if(numEmptyNeighbors == 0 && myCell.getFuturestate() != "NULL"){
-	 * myCell.setFuturestate(FISH); } else{ ArrayList<Integer> myFriends =
-	 * getNonEmptyNeighbors(myNeighbors, SHARK); direction =
-	 * generator.nextInt(myFriends.size());
-	 * //myCell.decrementReproductionTime(); //direction =
-	 * myNeighbors.get(direction); Fish cellToUpdate = (Fish)
-	 * myCell.getNeighbors().get(direction); } }
-	 * 
-	 * public void sharkMove(ArrayList<RectangleNoDiagonals> myNeighbors, Fish
-	 * myCell){ ArrayList<Integer> myFriends= getNonEmptyNeighbors(myNeighbors,
-	 * SHARK); changeCellToShark(myCell, myFriends); }
-	 * 
-	 * public ArrayList<Integer>
-	 * getNonEmptyNeighbors(ArrayList<RectangleNoDiagonals> myNeighbors, String
-	 * state){ ArrayList<Integer> myFriends = new ArrayList<Integer>(); for(int
-	 * i = 0; i<myNeighbors.size(); i++){
-	 * if(myNeighbors.get(i).getCurrentstate().equals(state)){ myFriends.add(i);
-	 * } } return myFriends; }
-	 * 
-	 * public int countEmpty(ArrayList<RectangleNoDiagonals> myNeighbors){ int
-	 * emptyCount = 0; for(Cell neighbor: myNeighbors){
-	 * if(neighbor.getCurrentstate() == EMPTY){ emptyCount++; } } return
-	 * emptyCount; }
-	 * 
-	 * public void transferInformation(Fish prevCell, Fish newCell){
-	 * newCell.setReproductionTime(prevCell.getReproductionTime());
-	 * newCell.setMaxReproductionTime(prevCell.getMaxReproductionTime());
-	 * newCell.setTimeToDeath(prevCell.getTimeToDeath()); }
-	 * 
-	 * //can't move somewhere with another shark public void
-	 * changeCellToShark(Fish myCell, ArrayList<Integer> myFishFriends){
-	 * //ArrayList<> int direction; //eats something if(myFishFriends.size() ==
-	 * 0){ if(myCell.getTimeToDeath() == 1){ myCell.setFuturestate(EMPTY);
-	 * //myCell.setCurrentstate(EMPTY); return; } ArrayList<Integer> emptyCells
-	 * = countCellsOfState(myCell.getNeighbors(myCell, myGrid), EMPTY);
-	 * if(emptyCells.size() == 0){ myCell.setFuturestate(SHARK); } direction =
-	 * generator.nextInt(emptyCells.size()); myCell.decrementReproductionTime();
-	 * } //doesn't eat something else{ if(myFishFriends.size() == 1){ direction
-	 * = myFishFriends.get(0); } else{ int directionIndex =
-	 * generator.nextInt(myFishFriends.size()); direction =
-	 * myFishFriends.get(directionIndex); } myCell.increaseTimeToDeath(); }
-	 * if(myCell.getReproductionTime() == 0){ giveBirth(myCell, SHARK); } else{
-	 * myCell.setCurrentstate(EMPTY); } Fish cellToUpdate = (Fish)
-	 * myCell.getNeighbors(myCell, myGrid).get(direction);
-	 * cellToUpdate.setFuturestate(SHARK); transferInformation(myCell,
-	 * cellToUpdate); }
-	 * 
-	 * public ArrayList<Integer>
-	 * countCellsOfState(ArrayList<RectangleNoDiagonals> currentNeighbors,
-	 * String state){ ArrayList<Integer> emptyCells = new ArrayList<Integer>();
-	 * for(int i = 0; i<currentNeighbors.size(); i++){
-	 * if(currentNeighbors.get(i).getCurrentstate().equals(state)){
-	 * emptyCells.add(i); } } return emptyCells; }
-	 * 
-	 * public void giveBirth(Fish myCell, String state){
-	 * myCell.setFuturestate(state); myCell.resetReproductionTime();
-	 * myCell.resetTimeToDeath(); }
-	 */
+//	public void printGrid(){
+//		Random generator = new Random();
+//		Cell[][] myGrid = getGrid();
+//		for (int i = 0; i < getNumRows(); i++) {
+//			for (int j = 0; j < getNumCols(); j++) {
+//				if(myGrid[i][j].getCurrentstate().equals(EMPTY)){
+//					System.out.print("E");
+//				}
+//				else if(myGrid[i][j].getCurrentstate().equals(FISH)){
+//					System.out.print("F");
+//				}
+//				else{
+//					System.out.print("S");
+//				}
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//	}
+//	
+//	public static void main(String[] args){
+//		PredatorPreySimulation test = new PredatorPreySimulation(2,2);
+//		test.createGrid();
+//		int num = 0;
+//		while(num<10){
+//			test.printGrid();
+//			test.updateGrid();
+//			num++;
+//		}
+//	}
 }
